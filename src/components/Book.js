a80b050faa3f29e03f3b6c03efec99512d71d42d
andykofi <andykofi2@gmail.com>
2020-07-12 14:48:29 +0100

    first commit

---
+++ src/components/Book.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Book extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Initial Point here</h1>
+            </div>
+        );
+    }
+}
+
+export default Book;
