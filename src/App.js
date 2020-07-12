4499c3098a5db42201bdf1514e178545dd57b4ff
andykofi <andykofi2@gmail.com>
2020-07-12 17:21:32 +0100

    save display

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+import './styles.scss';
+import Book from "./components/Book";
+import Pagination from "./components/Pagination";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [booksPerPage] = useState(10);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            setLoading(true);
+            const res = await axios.post("http://nyx.vima.ekt.gr:3000/api/books");
+            setBooks(res.data.books);
+            setLoading(false);
+        };
+
+        fetchBooks();
+    }, []);
+
+    // Get current books
+    const indexOfLastBook = currentPage * booksPerPage;
+    const indexOfFirstBook = indexOfLastBook - booksPerPage;
+    const currentPosts = books.slice(indexOfFirstBook, indexOfLastBook);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+    return (
+        <div className="container">
+            <Book books={currentPosts} loading={loading} />
+            <Pagination
+                booksPerPage={booksPerPage}
+                totalPosts={books.length}
+                paginate={paginate}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 
-class Book extends Component {
-    render() {
-        return (
-            <div>
-                <h1> Initial Point here</h1>
-            </div>
-        );
+const Book = ({books, loading}) => {
+    if(loading) {
+        return <h2>Loading...</h2>
     }
+
+    return (books.map((book) =>
+        <ListGroup className="text-primary" key={book.id}>
+            <ListGroup.Item>
+                <h4>{book.book_title}</h4>
+                <li>Author : {book.book_author}</li>
+                <li>Publication Year : {book.book_publication_year}</li>
+                <li>Publication Country : {book.book_publication_country}</li>
+                <li>Publication City : {book.book_publication_city}</li>
+                <li >Pages : {book.book_pages}</li>
+            </ListGroup.Item>
+        </ListGroup>
+    ));
 }
 
 export default Book;
