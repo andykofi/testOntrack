f76a9b46f8add5d95e35aa1624d314b92fc8bdb9
andykofi <andykofi2@gmail.com>
2020-07-13 17:54:21 +0100

    Implementing the search filter

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,31 +1,15 @@
 import React from "react";
-import { Button } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
 import { InputGroup } from 'react-bootstrap';
 import Declarations from '../components/Declarations'
 
 
 class Search extends React.Component {
-    updateTextInput(event) {
-        this.context.textInput = event.target.value;
-        if (event.key === 'Enter')
-            return this.context.searchFilters();
-    }
+
 
     render() {
         return (
             <InputGroup>
-                <FormControl
-                    type="text"
-                    placeholder="Search books"
-                    onChange={this.updateTextInput.bind(this)}
-                    onKeyDown={this.updateTextInput.bind(this)}
-                />
-                <InputGroup.Append>
-                    <Button onClick={this.context.searchFilters.bind(this)}>
-                        Search
-                    </Button>
-                </InputGroup.Append>
+
             </InputGroup>
         );
     }
